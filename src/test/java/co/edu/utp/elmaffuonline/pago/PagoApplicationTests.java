f30766f59d38178d62b1a71dad1bb3e297bb7c28
Miguel Cortes <andres.cortes@utp.edu.co>
2020-05-07 11:02:49 -0500

    version 1

---
+++ src/test/java/co/edu/utp/elmaffuonline/pago/PagoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.elmaffuonline.pago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
